1bc80119c2ff5e41b751097639d4efa095b465e0
Marcos Ramos <mksvncs@gmail.com>
2023-12-25 20:52:11 +0000

    ScrollSuave transformado em classe

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,17 +1,31 @@
-export default function initScrollSuave() {
-  const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
-
-  function scrollToSection(event) {
-    event.preventDefault();
-    const href = event.currentTarget.getAttribute('href');
-    const section = document.querySelector(href);
-    section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
+export default class ScrollSuave {
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
+    if (this.options === undefined) {
+      this.options = { behavior: "smooth", block: "start" };
+    } else {
+      this.options = options;
+    }
+    // Sempre que uma função for usada como callback, ela deve ser atribuida a ela mesma.bind(this) no corpo de construtor
+    this.scrollToSection = this.scrollToSection.bind(this);
   }
 
-  linksInternos.forEach((link) => {
-    link.addEventListener('click', scrollToSection);
-  });
+  scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView(this.options);
+  }
+
+  addLinkEvent() {
+    this.linksInternos.forEach((link) =>
+      link.addEventListener("click", this.scrollToSection)
+    );
+  }
+
+  init() {
+    if(this.linksInternos.length)
+    this.addLinkEvent();
+    return this;
+  }
 }
